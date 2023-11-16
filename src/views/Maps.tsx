d97601eb7952fd59517725d8d293854a5e51fc45
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-16 14:38:19 +0100

    Basic definition from views/Maps + Error in App.tsx fixed

---
+++ src/views/Maps.tsx
@@ -0,0 +1,11 @@
+import { ReactElement } from "react";
+import LastesSearches from "../components/LastestSearches";
+
+export default function Maps(): ReactElement {
+    return (
+        <div>
+            <h5>Maps</h5>
+            <LastesSearches />
+        </div>
+    )
+}
