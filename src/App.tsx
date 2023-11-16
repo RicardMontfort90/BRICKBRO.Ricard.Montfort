d461f2eded032822c97bb5181f51eec1b61b53b6
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-16 13:57:20 +0100

    create from folder views/ ErrorPage/MainPage/Map & from folder components/ Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { ReactElement } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import MainPage from 
 
-function App() {
+
+
+export default function App(): ReactElement {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
     </div>
   );
 }
-
-export default App;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,15 @@
 import { ReactElement } from "react";
 import { Link } from "react-router-dom";
-import brickbroLogo from "../images/BRICKBRO_LOGO.png;"
+import brickbroLogo from "../images/BRICKBRO_LOGO.png";
+
+
+export default function Navbar(): ReactElement {
+    return <div>Navbar view</div>;
+    return (
+        <div>
+            <Link to="/">
+                <img className="app-logo" src={brickbroLogo} alt="logo" />
+            </Link>
+        </div>
+    );
+}
