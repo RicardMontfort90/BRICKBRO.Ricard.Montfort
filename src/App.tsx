64631623980f0c86ab3851a96866d5b581832271
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-16 14:35:05 +0100

    Basic definition from views/MaingPage + Error in App.tsx fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { ReactElement } from 'react';
+import { ReactElement } from 'react';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
