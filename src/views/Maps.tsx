42af914340ab51b655d79972fbde66505dba06f1
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-17 10:13:08 +0100

    firsts steps with maps in src/views/Mpas.tsx

--- src/views/Maps.tsx
+++ src/views/Maps.tsx
@@ -1,11 +1,33 @@
 import { ReactElement } from "react";
 import LastesSearches from "../components/LastestSearches";
+import SearchBar from "../components/SearchBar";
+import { GoogleMap, LoadScript } from "@react-google-maps/api";
 
 export default function Maps(): ReactElement {
+    // Sagrada Familia, Barcelona
+    const sagradaFamilia = {
+        lat: 41.40366478082409,
+        lng: 2.1743983200647503
+    };
+    
+    // Maps Size
+    const mapsSize = {
+        height: '300px',
+        margin: '100px'
+    };
+
     return (
-        <div>
-            <h5>Maps</h5>
+        <>
+            <p>Welcome to the Map!</p>
+            <SearchBar />
+            <LoadScript googleMapsApiKey="YOUR_KEY">
+                <GoogleMap
+                    mapContainerStyle={mapsSize}
+                    zoom={20}
+                    center={sagradaFamilia}
+                ></GoogleMap>
+            </LoadScript>
             <LastesSearches />
-        </div>
-    )
-}
+        </>
+    );
+};
