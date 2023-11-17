49f49f3a5bc2456da3c65361c3e8a65cab2e06eb
RicardMontfort90 <ricard.montfort.romero@gmail.com>
2023-11-17 12:01:46 +0100

    Arrow function in src/views/ErrorPage.tsx

--- src/views/ErrorPage.tsx
+++ src/views/ErrorPage.tsx
@@ -1,11 +1,13 @@
-import { ReactElement } from 'react';
+import React, { ReactElement } from 'react';
 import { Link } from 'react-router-dom';
 
-export default function ErrorPage(): ReactElement {
+const ErrorPage: React.FC = (): ReactElement => {
     return (
         <div>
             <h5> hhhmmm... Esta página no existe... </h5>
             <Link to='/'>Volver...</Link>
         </div>
-    )
-}
+    );
+};
+
+export default ErrorPage;
